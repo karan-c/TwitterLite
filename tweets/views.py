948a4d4fa868bf88bfb3c5203085464904de6896
karan_c <karanchotaliya99@gmail.com>
2021-10-16 16:00:16 +0530

    started project and created tweets app

---
+++ tweets/views.py
@@ -0,0 +1,25 @@
+from django.http import JsonResponse
+from django.http.response import Http404
+from django.shortcuts import render
+
+from tweets.models import Tweet
+
+# Create your views here.
+def all_tweet_view(request, *args, **kwargs):
+		res_tweet = Tweet.objects.get(id=3)
+
+		return JsonResponse(res_tweet)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+		res = {
+				"id": tweet_id,
+		}
+		status = 200
+		try: 
+			tweet_obj = Tweet.objects.get(id=tweet_id)
+			res['content'] = tweet_obj.content
+		except:
+			status = 404
+			res['message'] = "Tweet not found"
+		
+		return JsonResponse(res, status=status)
