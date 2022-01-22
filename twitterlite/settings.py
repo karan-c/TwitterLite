11f79c10c5ffe9739e7fa6173978eea7934b653e
karan_c <karanchotaliya99@gmail.com>
2022-01-22 16:46:39 +0530

    heroku config vars setup

--- twitterlite/settings.py
+++ twitterlite/settings.py
@@ -12,20 +12,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import environ
 import os
+import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -156,3 +155,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 IMGUR_CLIENT_ID = os.environ['IMGUR_CLIENT_ID']
+# IMGUR_CLIENT_ID = config('IMGUR_CLIENT_ID')
+# Activate Django-Heroku.
+django_heroku.settings(locals())
